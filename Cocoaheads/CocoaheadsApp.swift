f1f8e07a115a2e1ca59d18d493658bc2d7a8365c
Charlie Minow <minow@mac.com>
2026-07-28 13:55:25 -0600

    Initial Commit

---
+++ Cocoaheads/CocoaheadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaheadsApp.swift
+//  Cocoaheads
+//
+//  Created by Charlie Minow on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct CocoaheadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CocoaheadsTests/CocoaheadsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaheadsTests.swift
+//  CocoaheadsTests
+//
+//  Created by Charlie Minow on 7/28/26.
+//
+
+import Testing
+@testable import Cocoaheads
+
+struct CocoaheadsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
